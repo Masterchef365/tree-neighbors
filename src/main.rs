dedb0df0080e87df20e24ef76f747d4531416207
Masterchef365 <duncan.freeman1@gmail.com>
2025-06-29 20:48:43 -0700

    on that furry shit

--- src/main.rs
+++ src/main.rs
@@ -35,9 +35,8 @@ fn random_tree(p: f64, parent: Option<NodeRef>) -> NodeRef {
     }));
 
     if rand::thread_rng().gen_bool(p.min(1.0)) {
-        let content = NodeContent::Branch(
-            [(); 4].map(|_| random_tree(p / 4.0, Some(new_node.clone()))),
-        );
+        let content =
+            NodeContent::Branch([(); 4].map(|_| random_tree(p / 4.0, Some(new_node.clone()))));
         new_node.borrow_mut().content = content;
     }
 
@@ -82,9 +81,7 @@ fn draw_tree_recursive(ui: &mut Ui, node: &NodeRef, rect: Rect) {
 
 fn find_node_recursive(pos: Pos2, node: NodeRef, rect: Rect) -> Option<NodeRef> {
     match &node.borrow().content {
-        NodeContent::Leaf(_) => {
-            rect.contains(pos).then(|| node.clone())
-        }
+        NodeContent::Leaf(_) => rect.contains(pos).then(|| node.clone()),
         NodeContent::Branch(branches) => {
             let (lefts, rights) = rect.split_left_right_at_fraction(0.5);
             let (top_left, bottom_left) = lefts.split_top_bottom_at_fraction(0.5);
@@ -111,6 +108,146 @@ fn zero_leaves(node: &NodeRef) {
             for branch in branches {
                 zero_leaves(branch);
             }
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[repr(usize)]
+enum Quadrant {
+    TopLeft = 0,
+    TopRight = 1,
+    BotLeft = 2,
+    BotRight = 3,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[repr(usize)]
+enum Edge {
+    Top = 0,
+    Bottom = 1,
+    Left = 2,
+    Right = 3,
+}
+
+impl Edge {
+    fn neighbor_quadrants(&self) -> [Quadrant; 2] {
+        match self {
+            Edge::Top => [Quadrant::BotLeft, Quadrant::BotRight],
+            Edge::Bottom => [Quadrant::TopLeft, Quadrant::TopRight],
+            Edge::Left => [Quadrant::TopRight, Quadrant::BotRight],
+            Edge::Right => [Quadrant::TopLeft, Quadrant::BotLeft],
+        }
+    }
+}
+
+impl Quadrant {
+    fn mirror(&self, edge: Edge) -> Self {
+        let horiz = matches!(edge, Edge::Left | Edge::Right);
+
+        match (self, horiz) {
+            (Self::TopLeft, true) => Self::BotRight,
+            (Self::TopRight, true) => Self::BotLeft,
+            (Self::BotLeft, true) => Self::TopRight,
+            (Self::BotRight, true) => Self::TopLeft,
+
+            (Self::TopLeft, false) => Self::TopRight,
+            (Self::TopRight, false) => Self::TopLeft,
+            (Self::BotLeft, false) => Self::BotRight,
+            (Self::BotRight, false) => Self::BotLeft,
+        }
+    }
+}
+/*
+impl Quadrant {
+    /// At this quadrant, which edges are shared by this quadrant in the same branch?
+    fn adjacent_edges(&self) -> [Edge; 2] {
+        match self {
+            Self::TopLeft => [Edge::Right, Edge::Bottom],
+            Self::TopRight => [Edge::Left, Edge::Bottom],
+            Self::BotLeft => [Edge::Right, Edge::Top],
+            Self::BotRight => [Edge::Left, Edge::Top],
+        }
+    }
+}
+*/
+
+fn adjacent_quadrant(quad: Quadrant, edge: Edge) -> Option<Quadrant> {
+    match (quad, edge) {
+        (Quadrant::TopLeft, Edge::Right) => Some(Quadrant::TopRight),
+        (Quadrant::BotLeft, Edge::Right) => Some(Quadrant::BotRight),
+
+        (Quadrant::TopRight, Edge::Left) => Some(Quadrant::TopLeft),
+        (Quadrant::BotRight, Edge::Left) => Some(Quadrant::BotLeft),
+
+        (Quadrant::TopLeft, Edge::Bottom) => Some(Quadrant::BotLeft),
+        (Quadrant::TopRight, Edge::Bottom) => Some(Quadrant::BotRight),
+
+        (Quadrant::BotLeft, Edge::Top) => Some(Quadrant::TopLeft),
+        (Quadrant::BotRight, Edge::Top) => Some(Quadrant::TopRight),
+        _ => None,
+    }
+}
+
+fn neighbor_func(
+    node: &NodeRef,
+    edge: Edge,
+    mut up_tracking: Vec<Quadrant>,
+    f: &mut impl FnMut(NodeRef),
+) {
+    let borrow = node.borrow();
+    let Some(parent) = borrow.parent.as_ref() else {
+        // Root has no neighbors
+        return;
+    };
+    let NodeContent::Branch(branches) = &parent.borrow().content else {
+        panic!("Invalid tree; parent node is a leaf!");
+    };
+
+    let quad = branches
+        .iter()
+        .position(|branch| Rc::ptr_eq(&node, branch))
+        .map(Quadrant::from)
+        .expect("Parent did not contain child");
+
+    up_tracking.push(quad);
+
+    if let Some(adj) = adjacent_quadrant(quad, edge) {
+        down_func(&branches[adj as usize], edge, up_tracking, f);
+    } else {
+        neighbor_func(parent, edge, up_tracking, f);
+    }
+}
+
+fn down_func(
+    node: &NodeRef,
+    edge: Edge,
+    mut up_tracking: Vec<Quadrant>,
+    f: &mut impl FnMut(NodeRef),
+) {
+    let borrow = node.borrow();
+    match &borrow.content {
+        NodeContent::Leaf(_) => f(node.clone()),
+        NodeContent::Branch(branches) => {
+            if let Some(quad) = up_tracking.pop() {
+                down_func(&branches[quad as usize], edge, up_tracking, f);
+            } else {
+                for quad in edge.neighbor_quadrants() {
+                    down_func(&branches[quad as usize], edge, up_tracking.clone(), f);
+                }
+            }
+        }
+    }
+}
+
+impl From<usize> for Quadrant {
+    fn from(value: usize) -> Self {
+        match value {
+            0 => Quadrant::TopLeft,
+            1 => Quadrant::TopRight,
+            2 => Quadrant::BotLeft,
+            3 => Quadrant::BotRight,
+            _ => panic!("Incorrect quadrant index"),
         }
     }
 }
